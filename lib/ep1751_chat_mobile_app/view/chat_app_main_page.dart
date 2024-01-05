fd5ebcc514b78f9b1bf5a28c9dc050d715ffc3e7
JAICHANGPARK <aristojeff@gmail.com>
2024-01-05 19:05:03 +0900

    Update chat_app_main_page.dart

---
+++ lib/ep1751_chat_mobile_app/view/chat_app_main_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class ChatAppMainPage extends StatefulWidget {
+  const ChatAppMainPage({super.key});
+
+  @override
+  State<ChatAppMainPage> createState() => _ChatAppMainPageState();
+}
+
+class _ChatAppMainPageState extends State<ChatAppMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
