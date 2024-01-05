1e526703039c01988fe0ec029b4fe8d35b618aaf
JAICHANGPARK <aristojeff@gmail.com>
2024-01-05 19:03:40 +0900

    Create ep1751_main.dart

---
+++ lib/ep1751_chat_mobile_app/ep1751_main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class ChatMobileApp extends StatelessWidget {
+  const ChatMobileApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
