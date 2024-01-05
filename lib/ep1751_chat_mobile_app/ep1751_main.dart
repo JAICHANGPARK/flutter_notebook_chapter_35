baa1280234822e994b3fd678bb5c23a40b99c445
JAICHANGPARK <aristojeff@gmail.com>
2024-01-05 19:07:24 +0900

    Update ep1751_main.dart

--- lib/ep1751_chat_mobile_app/ep1751_main.dart
+++ lib/ep1751_chat_mobile_app/ep1751_main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_notebook_chapter_35/ep1751_chat_mobile_app/view/chat_app_main_page.dart';
 
 void main() {
   runApp(ChatMobileApp());
@@ -10,7 +11,7 @@ class ChatMobileApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      
+      home: ChatAppMainPage(),
     );
   }
 }
