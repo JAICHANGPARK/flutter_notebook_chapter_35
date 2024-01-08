f5e98454e9a62b46ba624883ef95eafcd7799883
JAICHANGPARK <aristojeff@gmail.com>
2024-01-08 17:38:29 +0900

    Update ep1754_main.dart

--- lib/ep1754_on_demand_app/ep1754_main.dart
+++ lib/ep1754_on_demand_app/ep1754_main.dart
@@ -1,1 +1,11 @@
 import 'package:flutter/material.dart';
+
+
+class OnDemandServiceApp extends StatelessWidget {
+  const OnDemandServiceApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
