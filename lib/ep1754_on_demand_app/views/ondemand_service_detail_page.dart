82ea09a83592bacf61e6ca7a817ad20cf8de8b49
JAICHANGPARK <aristojeff@gmail.com>
2024-01-10 19:46:41 +0900

    Update ondemand_service_detail_page.dart

--- lib/ep1754_on_demand_app/views/ondemand_service_detail_page.dart
+++ lib/ep1754_on_demand_app/views/ondemand_service_detail_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class OnDemandServiceDetailPage extends StatefulWidget {
   const OnDemandServiceDetailPage({super.key});
 
@@ -10,13 +9,35 @@ class OnDemandServiceDetailPage extends StatefulWidget {
 
 class _OnDemandServiceDetailPageState extends State<OnDemandServiceDetailPage> {
   @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text("Continue"),
+      ),
+      
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
         children: [
           Container(
-            height: 180,
-          )
+            height: 240,
+            color: Colors.tealAccent,
+          ),
+          Text("Select your services"),
+          Expanded(
+            child: ListView.builder(
+              padding: EdgeInsets.zero,
+              itemBuilder: (context, index) {
+                return Placeholder();
+              },
+            ),
+          ),
         ],
       ),
     );
