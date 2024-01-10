c92af371a96b4b6cabb5c27fdd84c605d9521f67
JAICHANGPARK <aristojeff@gmail.com>
2024-01-10 19:47:19 +0900

    Update ondemand_service_detail_page.dart

--- lib/ep1754_on_demand_app/views/ondemand_service_detail_page.dart
+++ lib/ep1754_on_demand_app/views/ondemand_service_detail_page.dart
@@ -12,13 +12,19 @@ class _OnDemandServiceDetailPageState extends State<OnDemandServiceDetailPage> {
   void initState() {
     // TODO: implement initState
     super.initState();
-    ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(
-        content: Text("Continue"),
-      ),
-      
-    );
+    Future.delayed(Duration(microseconds: 1), (){
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text("Continue"),
+          duration: Duration(days: 1),
+        ),
+
+      );
+    });
+
   }
+
+
 
   @override
   Widget build(BuildContext context) {
