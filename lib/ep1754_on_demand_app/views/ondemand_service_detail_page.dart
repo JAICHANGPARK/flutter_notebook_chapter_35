86b362cd51e529b17ecf72fd1c8a132599e811e2
JAICHANGPARK <aristojeff@gmail.com>
2024-01-10 19:43:45 +0900

    Create ondemand_service_detail_page.dart

---
+++ lib/ep1754_on_demand_app/views/ondemand_service_detail_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class OnDemandServiceDetailPage extends StatefulWidget {
+  const OnDemandServiceDetailPage({super.key});
+
+  @override
+  State<OnDemandServiceDetailPage> createState() => _OnDemandServiceDetailPageState();
+}
+
+class _OnDemandServiceDetailPageState extends State<OnDemandServiceDetailPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
