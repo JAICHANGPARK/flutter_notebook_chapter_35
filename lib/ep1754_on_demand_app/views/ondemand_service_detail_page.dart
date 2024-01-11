a288eda41728c6b1cba88d96cb04d57c83d540b8
JAICHANGPARK <aristojeff@gmail.com>
2024-01-11 18:29:05 +0900

    Update ondemand_service_detail_page.dart

--- lib/ep1754_on_demand_app/views/ondemand_service_detail_page.dart
+++ lib/ep1754_on_demand_app/views/ondemand_service_detail_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 
 class OnDemandServiceDetailPage extends StatefulWidget {
   const OnDemandServiceDetailPage({super.key});
@@ -29,17 +30,35 @@ class _OnDemandServiceDetailPageState extends State<OnDemandServiceDetailPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Container(
             height: 240,
             color: Colors.tealAccent,
-          ),
-          Text(
-            "Select your services",
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
+            child: SafeArea(
+              child: Stack(
+                children: [
+                  Row(
+                    children: [
+                      
+                    ],
+                  )
+                ],
+              ),
             ),
           ),
+          Gap(16),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: Text(
+              "Select your services",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 18
+              ),
+            ),
+          ),
+          Gap(16),
           Expanded(
             child: ListView.builder(
               padding: EdgeInsets.zero,
