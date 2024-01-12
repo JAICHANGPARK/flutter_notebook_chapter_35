d078e1897a15876d605a1d4e8743e01e68ca8658
JAICHANGPARK <aristojeff@gmail.com>
2024-01-12 17:16:26 +0900

    Update ep1758_main.dart

---
+++ lib/ep1758_fitness_app/ep1758_main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class FitnessMobileApp extends StatelessWidget {
+  const FitnessMobileApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
