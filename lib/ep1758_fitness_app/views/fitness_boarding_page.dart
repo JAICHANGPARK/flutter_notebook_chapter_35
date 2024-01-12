f7350f1aa33a590e3102751df2ee9e6d6b270840
JAICHANGPARK <aristojeff@gmail.com>
2024-01-12 17:17:46 +0900

    Create fitness_boarding_page.dart

---
+++ lib/ep1758_fitness_app/views/fitness_boarding_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class FitnessBoardingPage extends StatefulWidget {
+  const FitnessBoardingPage({super.key});
+
+  @override
+  State<FitnessBoardingPage> createState() => _FitnessBoardingPageState();
+}
+
+class _FitnessBoardingPageState extends State<FitnessBoardingPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
