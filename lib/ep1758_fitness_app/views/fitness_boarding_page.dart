f5855da9921aa419c3512811d7c21c72c58798a3
JAICHANGPARK <aristojeff@gmail.com>
2024-01-12 17:21:28 +0900

    Update fitness_boarding_page.dart

--- lib/ep1758_fitness_app/views/fitness_boarding_page.dart
+++ lib/ep1758_fitness_app/views/fitness_boarding_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 
 class FitnessBoardingPage extends StatefulWidget {
   const FitnessBoardingPage({super.key});
@@ -17,20 +18,26 @@ class _FitnessBoardingPageState extends State<FitnessBoardingPage> {
           child: Column(
             children: [
               Container(
-                height: 320,
+                height: 360,
+                margin: EdgeInsets.only(left: 24,right: 24),
                 child: Placeholder(),
               ),
+              Gap(16),
               Expanded(
                 child: Container(
+                  margin: EdgeInsets.only(
+                    left: 24,
+                  ),
                   decoration: BoxDecoration(
                     borderRadius: BorderRadius.only(
                       topLeft: Radius.circular(24),
                       bottomLeft: Radius.circular(24),
                     ),
-                    
+                    color: Colors.tealAccent,
                   ),
                 ),
               ),
+              Gap(16),
               Container(
                 height: 72,
                 decoration: BoxDecoration(),
