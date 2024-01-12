23d05a9fae879afdd31e7020228dba36d079e655
JAICHANGPARK <aristojeff@gmail.com>
2024-01-12 17:18:52 +0900

    Update fitness_boarding_page.dart

--- lib/ep1758_fitness_app/views/fitness_boarding_page.dart
+++ lib/ep1758_fitness_app/views/fitness_boarding_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class FitnessBoardingPage extends StatefulWidget {
   const FitnessBoardingPage({super.key});
 
@@ -11,6 +10,18 @@ class FitnessBoardingPage extends StatefulWidget {
 class _FitnessBoardingPageState extends State<FitnessBoardingPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            Container(
+              height: 300,
+              child: Placeholder(),
+            ),
+            Expanded(child: Placeholder()),
+          ],
+        ),
+      ),
+    );
   }
 }
