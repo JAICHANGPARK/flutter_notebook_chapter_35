446216a70c1eac13929db7a82061bc5b43b8adbd
JAICHANGPARK <aristojeff@gmail.com>
2024-01-14 08:48:35 +0900

    Update fitness_home_page.dart

--- lib/ep1758_fitness_app/views/fitness_home_page.dart
+++ lib/ep1758_fitness_app/views/fitness_home_page.dart
@@ -11,6 +11,7 @@ class _FitnessHomePageState extends State<FitnessHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       body: SafeArea(
         child: Stack(
           children: [
