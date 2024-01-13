bb40308d12ff907231c326beaca484eb8c5f97ad
JAICHANGPARK <aristojeff@gmail.com>
2024-01-14 08:41:40 +0900

    Update fitness_home_page.dart

---
+++ lib/ep1758_fitness_app/views/fitness_home_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class FitnessHomePage extends StatefulWidget {
+  const FitnessHomePage({super.key});
+
+  @override
+  State<FitnessHomePage> createState() => _FitnessHomePageState();
+}
+
+class _FitnessHomePageState extends State<FitnessHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
