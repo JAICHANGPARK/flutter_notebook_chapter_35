a7b826d6f51b28b8c33cb357a817e17e6ab75e0e
JAICHANGPARK <aristojeff@gmail.com>
2024-01-17 18:49:12 +0900

    Update bag_shop_home_page.dart

--- lib/ep1763_bag_shop_app/views/bag_shop_home_page.dart
+++ lib/ep1763_bag_shop_app/views/bag_shop_home_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class BagShopHomePage extends StatefulWidget {
   const BagShopHomePage({super.key});
 
@@ -12,9 +11,26 @@ class _BagShopHomePageState extends State<BagShopHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.black,
       body: Column(
         children: [
-          
+          Row(
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.white,
+                  ),
+                ),
+                child: IconButton(
+                  onPressed: () {},
+                  icon: Icon(
+                    Icons.search,
+                  ),
+                ),
+              )
+            ],
+          )
         ],
       ),
     );
