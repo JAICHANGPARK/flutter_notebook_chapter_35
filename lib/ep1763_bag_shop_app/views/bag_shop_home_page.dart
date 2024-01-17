1b5267b2581d599d358b2dd700f52c4074add434
JAICHANGPARK <aristojeff@gmail.com>
2024-01-17 18:47:26 +0900

    Create bag_shop_home_page.dart

---
+++ lib/ep1763_bag_shop_app/views/bag_shop_home_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+
+class BagShopHomePage extends StatefulWidget {
+  const BagShopHomePage({super.key});
+
+  @override
+  State<BagShopHomePage> createState() => _BagShopHomePageState();
+}
+
+class _BagShopHomePageState extends State<BagShopHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          
+        ],
+      ),
+    );
+  }
+}
