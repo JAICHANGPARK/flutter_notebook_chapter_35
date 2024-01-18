6ef71a9fe3a53a5aeca2a433b0628db99dec3575
JAICHANGPARK <aristojeff@gmail.com>
2024-01-18 18:39:04 +0900

    Update ep1763_main.dart

--- lib/ep1763_bag_shop_app/ep1763_main.dart
+++ lib/ep1763_bag_shop_app/ep1763_main.dart
@@ -10,7 +10,7 @@ class BagShopApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       home: BagShopHomePage(),
     );
   }
