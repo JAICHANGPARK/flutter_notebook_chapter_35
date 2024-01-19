69f6b9e2190f906cad55f53074b279d23e7631e0
JAICHANGPARK <aristojeff@gmail.com>
2024-01-19 18:24:48 +0900

    Update bag_shop_detail_page.dart

--- lib/ep1763_bag_shop_app/views/bag_shop_detail_page.dart
+++ lib/ep1763_bag_shop_app/views/bag_shop_detail_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 
 class BagShopDetailPage extends StatefulWidget {
   const BagShopDetailPage({super.key});
@@ -15,7 +16,66 @@ class _BagShopDetailPageState extends State<BagShopDetailPage> {
       body: SafeArea(
         child: Column(
           children: [
-
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Row(
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                        border: Border.all(
+                          color: Colors.white,
+                        ),
+                        shape: BoxShape.circle),
+                    child: IconButton(
+                      onPressed: () {},
+                      icon: const Icon(
+                        Icons.arrow_back,
+                      ),
+                      color: Colors.white,
+                    ),
+                  ),
+                  const Expanded(
+                    child: Center(
+                      child: Text(
+                        "Details",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                          fontSize: 18,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Stack(
+                    children: [
+                      Container(
+                        decoration: BoxDecoration(
+                            border: Border.all(
+                              color: Colors.white,
+                            ),
+                            shape: BoxShape.circle),
+                        child: IconButton(
+                          onPressed: () {},
+                          icon: const Icon(
+                            Icons.person_outline,
+                          ),
+                          color: Colors.white,
+                        ),
+                      ),
+                      const Positioned(
+                        right: 0,
+                        top: 0,
+                        child: CircleAvatar(
+                          radius: 6,
+                          backgroundColor: Colors.orange,
+                        ),
+                      )
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            const Gap(16),
           ],
         ),
       ),
