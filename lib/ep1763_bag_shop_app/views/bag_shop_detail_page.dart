132edfaf2b78931b4539585e07e59ddcf214e95c
JAICHANGPARK <aristojeff@gmail.com>
2024-01-20 09:42:47 +0900

    Update bag_shop_detail_page.dart

--- lib/ep1763_bag_shop_app/views/bag_shop_detail_page.dart
+++ lib/ep1763_bag_shop_app/views/bag_shop_detail_page.dart
@@ -107,7 +107,17 @@ class _BagShopDetailPageState extends State<BagShopDetailPage> {
                 ],
               ),
             ),
-            const Expanded(child: Placeholder()),
+            Expanded(
+                child: Container(
+              decoration: BoxDecoration(
+                  color: Colors.green,
+                  image: DecorationImage(
+                    image: NetworkImage(
+                      "https://cdn.pixabay.com/photo/2017/04/05/01/12/traveler-2203666_1280.jpg",
+                    ),
+                    fit: BoxFit.cover,
+                  ),),
+            )),
             Gap(6),
             Container(
               height: 82,
