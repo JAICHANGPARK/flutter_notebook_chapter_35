b0d5e6325ec97ed1bd7b64c06adb325db158dc08
JAICHANGPARK <aristojeff@gmail.com>
2024-01-21 09:32:12 +0900

    Create banking_home_page.dart

---
+++ lib/ep1767_banking_app/views/banking_home_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class BankingHomePage extends StatefulWidget {
+  const BankingHomePage({super.key});
+
+  @override
+  State<BankingHomePage> createState() => _BankingHomePageState();
+}
+
+class _BankingHomePageState extends State<BankingHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
