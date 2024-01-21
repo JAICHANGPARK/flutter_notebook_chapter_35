27bf6544a2ff71c6a41863e4012f225e9a2301af
JAICHANGPARK <aristojeff@gmail.com>
2024-01-21 09:36:26 +0900

    Update banking_home_page.dart

--- lib/ep1767_banking_app/views/banking_home_page.dart
+++ lib/ep1767_banking_app/views/banking_home_page.dart
@@ -12,6 +12,21 @@ class _BankingHomePageState extends State<BankingHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color.fromRGBO(21, 23, 28, 1),
+      bottomNavigationBar: BottomAppBar(
+        child: Row(
+          children: [
+            Column(
+              children: [
+                Icon(Icons.home_filled),
+                CircleAvatar(
+                  radius: 4,
+                  backgroundColor: Color.fromRGBO(229, 76, 112, 1),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
     );
   }
 }
