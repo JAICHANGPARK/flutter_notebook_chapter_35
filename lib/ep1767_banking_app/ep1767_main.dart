dd4ad54f506c990f68a719a70e7f485e9cdc5d06
JAICHANGPARK <aristojeff@gmail.com>
2024-01-21 09:31:30 +0900

    Create ep1767_main.dart

---
+++ lib/ep1767_banking_app/ep1767_main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+
+void main() {
+  runApp(BankingApp());
+}
+
+class BankingApp extends StatelessWidget {
+  const BankingApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
