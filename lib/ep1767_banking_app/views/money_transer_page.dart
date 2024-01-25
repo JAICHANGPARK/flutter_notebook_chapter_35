39a04fc7859b10a6aa101d3892121e47fe50f018
JAICHANGPARK <aristojeff@gmail.com>
2024-01-25 19:38:00 +0900

    Update money_transer_page.dart

---
+++ lib/ep1767_banking_app/views/money_transer_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class MoneyTransferPage extends StatefulWidget {
+  const MoneyTransferPage({super.key});
+
+  @override
+  State<MoneyTransferPage> createState() => _MoneyTransferPageState();
+}
+
+class _MoneyTransferPageState extends State<MoneyTransferPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [],
+        ),
+      ),
+    );
+  }
+}
