7f8c1e4ae6c545065f44d6f80cf1d10f53d35023
JAICHANGPARK <aristojeff@gmail.com>
2024-01-28 10:03:59 +0900

    Update learning_home_page.dart

--- lib/ep1774_learning_app/view/learning_home_page.dart
+++ lib/ep1774_learning_app/view/learning_home_page.dart
@@ -14,25 +14,33 @@ class _LearningHomePageState extends State<LearningHomePage> {
       body: SafeArea(
         child: Column(
           children: [
-            Row(
-              children: [
-                Text("Cours"),
-                Text("e"),
-                Container(
-                  decoration: BoxDecoration(
-                    border: Border.all(
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                children: [
+                  Text("Cours"),
+                  Text("e"),
+                  Spacer(),
+                  Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.white,
+                      ),
+                      shape: BoxShape.circle,
+                    ),
+                    child: IconButton(
+                      icon: Icon(
+                        Icons.notifications_none,
+                      ),
+                      onPressed: () {},
                       color: Colors.white,
                     ),
-                    shape: BoxShape.circle,
                   ),
-                  child: IconButton(
-                    icon: Icon(
-                      Icons.notifications_none,
-                    ),
-                    onPressed: () {},
+                  CircleAvatar(
+                    radius: 26,
                   ),
-                )
-              ],
+                ],
+              ),
             )
           ],
         ),
