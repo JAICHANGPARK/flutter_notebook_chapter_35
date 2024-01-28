cf00f72c178ef1e51fda913e4102620001439521
JAICHANGPARK <aristojeff@gmail.com>
2024-01-28 09:59:07 +0900

    Create learning_home_page.dart

---
+++ lib/ep1774_learning_app/view/learning_home_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class LearningHomePage extends StatefulWidget {
+  const LearningHomePage({super.key});
+
+  @override
+  State<LearningHomePage> createState() => _LearningHomePageState();
+}
+
+class _LearningHomePageState extends State<LearningHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
