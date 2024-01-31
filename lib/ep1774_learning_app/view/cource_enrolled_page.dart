70ad9036e396d04c9a55cadc388e8c1c19911ba4
JAICHANGPARK <aristojeff@gmail.com>
2024-01-31 19:06:43 +0900

    Create cource_enrolled_page.dart

---
+++ lib/ep1774_learning_app/view/cource_enrolled_page.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class CourseEnrolledPage extends StatefulWidget {
+  const CourseEnrolledPage({super.key});
+
+  @override
+  State<CourseEnrolledPage> createState() => _CourseEnrolledPageState();
+}
+
+class _CourseEnrolledPageState extends State<CourseEnrolledPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            Text(
+              "Course you\nEnrolled",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
