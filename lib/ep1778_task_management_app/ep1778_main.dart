56eaac0c1c98a3796f6d6489908e0a5f983ab2a5
JAICHANGPARK <aristojeff@gmail.com>
2024-02-01 23:21:20 +0900

    Update ep1778_main.dart

--- lib/ep1778_task_management_app/ep1778_main.dart
+++ lib/ep1778_task_management_app/ep1778_main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
-
+void main() {
+  runApp(TaskManagementApp());
+}
 
 class TaskManagementApp extends StatelessWidget {
   const TaskManagementApp({super.key});
