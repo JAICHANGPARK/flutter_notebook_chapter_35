764e2c83dfe117952d38d736233fefacfce96eb7
JAICHANGPARK <aristojeff@gmail.com>
2024-02-01 23:23:30 +0900

    Update task_management_home_page.dart

--- lib/ep1778_task_management_app/view/task_management_home_page.dart
+++ lib/ep1778_task_management_app/view/task_management_home_page.dart
@@ -12,6 +12,12 @@ class TaskHomePage extends StatefulWidget {
 class _TaskHomePageState extends State<TaskHomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SafeArea(
+      child: Stack(
+        children: [
+          
+        ],
+      ),
+    );
   }
 }
