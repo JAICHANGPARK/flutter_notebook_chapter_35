e2375e70ca9cc50561b2629037650746958d3253
JAICHANGPARK <aristojeff@gmail.com>
2024-02-01 23:22:24 +0900

    Update task_management_home_page.dart

---
+++ lib/ep1778_task_management_app/view/task_management_home_page.dart
@@ -0,0 +1,17 @@
+
+
+import 'package:flutter/material.dart';
+
+class TaskHomePage extends StatefulWidget {
+  const TaskHomePage({super.key});
+
+  @override
+  State<TaskHomePage> createState() => _TaskHomePageState();
+}
+
+class _TaskHomePageState extends State<TaskHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
