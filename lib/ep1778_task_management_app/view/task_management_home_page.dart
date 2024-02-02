1d4855021d4d7af8f096c5fa317418509a0d4b80
JAICHANGPARK <aristojeff@gmail.com>
2024-02-03 08:59:18 +0900

    Update task_management_home_page.dart

--- lib/ep1778_task_management_app/view/task_management_home_page.dart
+++ lib/ep1778_task_management_app/view/task_management_home_page.dart
@@ -152,6 +152,11 @@ class _TaskHomePageState extends State<TaskHomePage> {
                                   ),
                                 ),
                               ],
+                            ),
+                            Text(
+                              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. D",
+                              maxLines: 2,
+                              overflow: TextOverflow.ellipsis,
                             )
                           ],
                         ),
