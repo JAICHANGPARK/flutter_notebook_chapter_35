29858509f4e6fd56a12b04a9d43cb37b9e863795
JAICHANGPARK <aristojeff@gmail.com>
2024-02-04 09:17:33 +0900

    Create task_detail_page.dart

---
+++ lib/ep1778_task_management_app/view/task_detail_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class TaskDetailPage extends StatefulWidget {
+  const TaskDetailPage({super.key});
+
+  @override
+  State<TaskDetailPage> createState() => _TaskDetailPageState();
+}
+
+class _TaskDetailPageState extends State<TaskDetailPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
