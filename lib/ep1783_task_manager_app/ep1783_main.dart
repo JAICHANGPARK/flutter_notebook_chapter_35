30dcfef19efbe223f0982d628cf666ee8a5da29e
JAICHANGPARK <aristojeff@gmail.com>
2024-02-06 18:35:58 +0900

    Update ep1783_main.dart

--- lib/ep1783_task_manager_app/ep1783_main.dart
+++ lib/ep1783_task_manager_app/ep1783_main.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 
+void main() {
+  runApp(TaskManagerApp());
+}
 
 class TaskManagerApp extends StatelessWidget {
   const TaskManagerApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp();
   }
 }
