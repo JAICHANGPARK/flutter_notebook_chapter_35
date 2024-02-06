20f511e37463f280284bec29595ea6604437114e
JAICHANGPARK <aristojeff@gmail.com>
2024-02-06 18:39:24 +0900

    Update task_manager_homa_page.dart

--- lib/ep1783_task_manager_app/view/task_manager_homa_page.dart
+++ lib/ep1783_task_manager_app/view/task_manager_homa_page.dart
@@ -11,6 +11,7 @@ class _TaskManagerHomePageState extends State<TaskManagerHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.black,
       body: SafeArea(
         child: Column(
           children: [
