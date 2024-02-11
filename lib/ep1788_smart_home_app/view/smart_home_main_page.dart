c947c4bdbf80af7b579fc7ee16fd6bf63142a0da
JAICHANGPARK <aristojeff@gmail.com>
2024-02-11 09:16:32 +0900

    Create smart_home_main_page.dart

---
+++ lib/ep1788_smart_home_app/view/smart_home_main_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class SmartHomeMainPage extends StatefulWidget {
+  const SmartHomeMainPage({super.key});
+
+  @override
+  State<SmartHomeMainPage> createState() => _SmartHomeMainPageState();
+}
+
+class _SmartHomeMainPageState extends State<SmartHomeMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
