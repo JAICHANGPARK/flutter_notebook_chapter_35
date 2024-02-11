5a4e114e97262b8d67a8351c38d59b54db02d672
JAICHANGPARK <aristojeff@gmail.com>
2024-02-11 09:15:43 +0900

    Update ep1788_main.dart

---
+++ lib/ep1788_smart_home_app/ep1788_main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+
+class SmartHomeApp extends StatelessWidget {
+  const SmartHomeApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: ,
+    );
+  }
+}
