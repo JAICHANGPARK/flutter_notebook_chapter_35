e5eb3a71399a35f164d6547d71dad8a9b5953db6
JAICHANGPARK <aristojeff@gmail.com>
2024-02-13 18:46:33 +0900

    Update smart_home_main_page.dart

--- lib/ep1788_smart_home_app/view/smart_home_main_page.dart
+++ lib/ep1788_smart_home_app/view/smart_home_main_page.dart
@@ -180,134 +180,137 @@ class _SmartHomeMainPageState extends State<SmartHomeMainPage> {
                         const Gap(8),
                       ],
                     ),
-                    Expanded(child: SingleChildScrollView(child:
-                      Column(
-                        children: [
-                          
-                        ],
-                      ),))
-                    const Gap(24),
-                    Container(
-                      decoration: BoxDecoration(
-                        color: Colors.white.withOpacity(.5),
-                        borderRadius: BorderRadius.circular(12),
-                      ),
-                      padding: const EdgeInsets.all(16),
-                      child: const Column(
-                        children: [
-                          Row(
-                            children: [
-                              Text(
-                                "Summary for ",
+                    Expanded(
+                      child: SingleChildScrollView(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const Gap(24),
+                            Container(
+                              decoration: BoxDecoration(
+                                color: Colors.white.withOpacity(.5),
+                                borderRadius: BorderRadius.circular(12),
                               ),
-                              Text(
-                                "July ",
-                              ),
-                              Spacer(),
-                              Icon(
-                                Icons.share,
-                              )
-                            ],
-                          ),
-                          Row(
-                            children: [
-                              Expanded(
-                                  child: Column(
+                              padding: const EdgeInsets.all(16),
+                              child: const Column(
                                 children: [
-                                  Text("\$401.76"),
                                   Row(
-                                    children: [Text("+198.01"), Gap(4), Text("-67.22%")],
+                                    children: [
+                                      Text(
+                                        "Summary for ",
+                                      ),
+                                      Text(
+                                        "July ",
+                                      ),
+                                      Spacer(),
+                                      Icon(
+                                        Icons.share,
+                                      )
+                                    ],
+                                  ),
+                                  Row(
+                                    children: [
+                                      Expanded(
+                                          child: Column(
+                                        children: [
+                                          Text("\$401.76"),
+                                          Row(
+                                            children: [Text("+198.01"), Gap(4), Text("-67.22%")],
+                                          )
+                                        ],
+                                      ))
+                                    ],
                                   )
                                 ],
-                              ))
-                            ],
-                          )
-                        ],
+                              ),
+                            ),
+                            const Gap(24),
+                            const Row(
+                              children: [
+                                Text(
+                                  "Room ",
+                                  style: TextStyle(
+                                    color: Colors.grey,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 18,
+                                  ),
+                                ),
+                                Text(
+                                  "Usage",
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 18,
+                                  ),
+                                ),
+                                Spacer(),
+                                CircleAvatar(
+                                  radius: 4,
+                                  backgroundColor: Colors.pink,
+                                ),
+                                Gap(8),
+                                Text(
+                                  "Overused",
+                                  style: TextStyle(
+                                    color: Colors.grey,
+                                  ),
+                                ),
+                                Gap(12),
+                                CircleAvatar(
+                                  radius: 4,
+                                  backgroundColor: Colors.yellow,
+                                ),
+                                Gap(8),
+                                Text(
+                                  "Good Use",
+                                  style: TextStyle(
+                                    color: Colors.grey,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            const Gap(24),
+                            Container(
+                              height: 320,
+                              child: const Placeholder(),
+                            ),
+                            const Gap(24),
+                            Row(
+                              children: [
+                                const Text(
+                                  "Room ",
+                                  style: TextStyle(
+                                    color: Colors.grey,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 18,
+                                  ),
+                                ),
+                                const Text(
+                                  "Devices ",
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 18,
+                                  ),
+                                ),
+                                const Text(
+                                  "10 ",
+                                  style: TextStyle(
+                                    color: Colors.grey,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 18,
+                                  ),
+                                ),
+                                const Spacer(),
+                                TextButton(
+                                  onPressed: () {},
+                                  child: const Text("All Devices"),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
                       ),
-                    ),
-                    const Gap(24),
-                    const Row(
-                      children: [
-                        Text(
-                          "Room ",
-                          style: TextStyle(
-                            color: Colors.grey,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 18,
-                          ),
-                        ),
-                        Text(
-                          "Usage",
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 18,
-                          ),
-                        ),
-                        Spacer(),
-                        CircleAvatar(
-                          radius: 4,
-                          backgroundColor: Colors.pink,
-                        ),
-                        Gap(8),
-                        Text(
-                          "Overused",
-                          style: TextStyle(
-                            color: Colors.grey,
-                          ),
-                        ),
-                        Gap(12),
-                        CircleAvatar(
-                          radius: 4,
-                          backgroundColor: Colors.yellow,
-                        ),
-                        Gap(8),
-                        Text(
-                          "Good Use",
-                          style: TextStyle(
-                            color: Colors.grey,
-                          ),
-                        ),
-                      ],
-                    ),
-                    const Gap(24),
-                    Container(
-                      height: 320,
-                      child: const Placeholder(),
-                    ),
-                    const Gap(24),
-                    Row(
-                      children: [
-                        const Text(
-                          "Room ",
-                          style: TextStyle(
-                            color: Colors.grey,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 18,
-                          ),
-                        ),
-                        const Text(
-                          "Devices ",
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 18,
-                          ),
-                        ),
-                        const Text(
-                          "10 ",
-                          style: TextStyle(
-                            color: Colors.grey,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 18,
-                          ),
-                        ),
-                        const Spacer(),
-                        TextButton(
-                          onPressed: () {},
-                          child: const Text("All Devices"),
-                        ),
-                      ],
                     ),
                   ],
                 ),
