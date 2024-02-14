0b3e1175f909c7051dff72d5bb30b2e8268c8e83
JAICHANGPARK <aristojeff@gmail.com>
2024-02-14 18:40:58 +0900

    Create ep1790_main.dart

---
+++ lib/ep1790_coffee_delivery_app/ep1790_main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class CoffeeDeliveryApp extends StatelessWidget {
+  const CoffeeDeliveryApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
