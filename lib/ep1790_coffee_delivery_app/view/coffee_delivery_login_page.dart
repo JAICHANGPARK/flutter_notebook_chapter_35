6316df823f136df2e9cd8901eb4d2e53efb0e555
JAICHANGPARK <aristojeff@gmail.com>
2024-02-14 18:43:11 +0900

    Create coffee_delivery_login_page.dart

---
+++ lib/ep1790_coffee_delivery_app/view/coffee_delivery_login_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class CoffeeDeliveryLoginPage extends StatefulWidget {
+  const CoffeeDeliveryLoginPage({super.key});
+
+  @override
+  State<CoffeeDeliveryLoginPage> createState() => _CoffeeDeliveryLoginPageState();
+}
+
+class _CoffeeDeliveryLoginPageState extends State<CoffeeDeliveryLoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
