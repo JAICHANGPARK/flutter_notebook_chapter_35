08d2499d67410e594980cd72f18878389e0f3825
JAICHANGPARK <aristojeff@gmail.com>
2024-02-15 18:57:26 +0900

    Update coffee_delivery_home_page.dart

---
+++ lib/ep1790_coffee_delivery_app/view/coffee_delivery_home_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class CoffeeDeliveryHomePage extends StatefulWidget {
+  const CoffeeDeliveryHomePage({super.key});
+
+  @override
+  State<CoffeeDeliveryHomePage> createState() => _CoffeeDeliveryHomePageState();
+}
+
+class _CoffeeDeliveryHomePageState extends State<CoffeeDeliveryHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
