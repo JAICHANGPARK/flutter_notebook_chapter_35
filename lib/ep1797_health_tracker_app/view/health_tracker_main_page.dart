e5bb0289d6e2ba142bc6bdb7cda935e72de63d3f
JAICHANGPARK <aristojeff@gmail.com>
2024-02-20 23:19:56 +0900

    Create health_tracker_main_page.dart

---
+++ lib/ep1797_health_tracker_app/view/health_tracker_main_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+
+
+class HealthTrackerMainPage extends StatefulWidget {
+  const HealthTrackerMainPage({super.key});
+
+  @override
+  State<HealthTrackerMainPage> createState() => _HealthTrackerMainPageState();
+}
+
+class _HealthTrackerMainPageState extends State<HealthTrackerMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          
+        ],
+      ),
+    );
+  }
+}
