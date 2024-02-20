3b7d012dd275f15dae111f1d7e3df82dc7b68f78
JAICHANGPARK <aristojeff@gmail.com>
2024-02-20 23:17:14 +0900

    Create ep1797_main.dart

---
+++ lib/ep1797_health_tracker_app/ep1797_main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class HealthTrackerApp extends StatelessWidget {
+  const HealthTrackerApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
